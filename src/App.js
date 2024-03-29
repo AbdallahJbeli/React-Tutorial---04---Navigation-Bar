bfa2d01a15ed3d6da377f8b4c0eefb3511dbe88d
abdallah jbeli <abdallahjbeli55@gmail.com>
2024-03-29 17:08:12 +0100

    Commit message explaining the changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Content, Footer } from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <Navbar />
+      </div>
+      <div className='content'>
+        <Content />
+      </div>
+      <div className='footer'>
+        <Footer />
+      </div>
     </div>
   );
 }
